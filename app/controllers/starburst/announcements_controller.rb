ff11a427f7114bbd25ca77d2777ecad12df6a63a
Rhys Park <sales@igpython.co.uk>
2015-03-10 22:27:10 +0000

    Change Find and create to block so to_i is not called

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -1,16 +1,18 @@
 module Starburst
 	class AnnouncementsController < ApplicationController
 		def mark_as_read
-			announcement = Announcement.find(params[:id].to_i)
+			announcement = Announcement.find(params[:id])
 			if respond_to?(Starburst.current_user_method) && send(Starburst.current_user_method) && announcement
-				if AnnouncementView.where(user_id: send(Starburst.current_user_method), announcement_id: announcement).first_or_create(user_id: send(Starburst.current_user_method), announcement_id: announcement)
-					render :json => :ok
-				else
-					render json: nil, :status => :unprocessable_entity
+				if AnnouncementView.where(user_id: send(Starburst.current_user_method), announcement_id: announcement).first_or_create do |anon|
+					user_id = send(Starburst.current_user_method)
+					announcement_id = announcement
 				end
+				render :json => :ok
 			else
-					render json: nil, :status => :unprocessable_entity
+				render json: nil, :status => :unprocessable_entity
 			end
+		else
+			render json: nil, :status => :unprocessable_entity
 		end
 	end
 end
