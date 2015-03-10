3ce07c579db6c95647169cd96abca8fa248bb096
Rhys Park <sales@igpython.co.uk>
2015-03-10 21:18:18 +0000

    Field name fix

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -3,7 +3,7 @@ module Starburst
 		def mark_as_read
 			announcement = Announcement.find(params[:id].to_i)
 			if respond_to?(Starburst.current_user_method) && send(Starburst.current_user_method) && announcement
-				if AnnouncementView.where(user_id: send(Starburst.current_user_method), announcement: announcement).first_or_create(user_id: send(Starburst.current_user_method), announcement: announcement)
+				if AnnouncementView.where(user_id: send(Starburst.current_user_method), announcement_id: announcement).first_or_create(user_id: send(Starburst.current_user_method), announcement_id: announcement)
 					render :json => :ok
 				else
 					render json: nil, :status => :unprocessable_entity
