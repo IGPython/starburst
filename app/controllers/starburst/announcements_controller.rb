a46728b0cd5dc04fe305b7365ec06f97046ee708
Rhys Park <sales@igpython.co.uk>
2015-03-10 22:40:55 +0000

    Fix find_or_create code in controller

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -3,16 +3,14 @@ module Starburst
 		def mark_as_read
 			announcement = Announcement.find(params[:id])
 			if respond_to?(Starburst.current_user_method) && send(Starburst.current_user_method) && announcement
-				if AnnouncementView.where(user_id: send(Starburst.current_user_method), announcement_id: announcement).first_or_create do |anon|
-					user_id = send(Starburst.current_user_method)
-					announcement_id = announcement
+				if AnnouncementView.where(user_id: send(Starburst.current_user_method), announcement_id: announcement).first_or_create
+					render :json => :ok
+				else
+					render json: nil, :status => :unprocessable_entity
 				end
-				render :json => :ok
 			else
-				render json: nil, :status => :unprocessable_entity
+					render json: nil, :status => :unprocessable_entity
 			end
-		else
-			render json: nil, :status => :unprocessable_entity
 		end
 	end
 end
